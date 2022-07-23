ee634423b42f98ad8e0b1bf99ae609b1e0d25353
Kristina Brennan <96805863+thetinaest@users.noreply.github.com>
2022-07-23 11:22:36 -0500

    added typeDefs, withauth, created mutations file, componenets

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID
+        user: User
+    }
+    input saveBookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        addUser(username: String, email: String, password: String): Auth
+        login(email: String, password: String): Auth
+        saveBook(bookData: saveBookInput): User
+        removeBook(bookId: ID): User
+    }
+`
+
+module.exports = typeDefs
